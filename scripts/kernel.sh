8891228eddc58eb17d5408bfa12c4d432409c1f3
Tyler McAdams <tmcadams@Claudes-Mac-Pro-6.local>
2020-09-19 19:41:39 -0400

    ctm - add -J3 for makeopts

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -4,6 +4,8 @@ cp $SCRIPTS/scripts/kernel.config /mnt/gentoo/tmp/
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
 echo 'ACCEPT_LICENSE="*"' >> /etc/portage/make.conf
+echo 'MAKEOPTS="-j3"' >> /etc/portage/make.conf
+cat /etc/portage/make.conf
 # echo "" >> /etc/portage/make.conf
 # echo "" >> /etc/portage/make.conf
 emerge sys-kernel/gentoo-sources
