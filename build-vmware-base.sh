31b5f07e047771acce53c060f52f1acf5fcfc2aa
Tyler McAdams <tmcadams@Claudes-Mac-Pro-6.local>
2020-09-20 19:47:03 -0400

    ctm - add packer command to build generic artifact

--- build-vmware-base.sh
+++ build-vmware-base.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 
+packer build -var cpu_brand="generic" \
+-var virt_type="vmware"         \
+-var march="native"             \
+-var make_opts="3"              \
+-var build_type="base"          \
+-var admin_user="$USER"         \
+-var stage3="20200916T214503Z"  \
+-var output_directory="/Users/$USER/gentoo" ./vmware.json
