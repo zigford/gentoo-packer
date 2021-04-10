aba2e2c1f2b38e4de58235050fa6f03cd4bb0e72
TyMac <TyMac@users.noreply.github.com>
2021-04-10 02:49:48 -0400

    Update build-vmware-base.sh
    add initial arg support to build script

--- build-vmware-base.sh
+++ build-vmware-base.sh
@@ -21,18 +21,19 @@ rm digest.asc
 # cat | grep -v -e hardened -e uclib -e nomultilib -e systemd -e x32 -e vanilla| awk '{print $1}' | tail -n1)
 
 # wget $base_url/$(curl $base_url/latest-stage3-amd64.txt | grep -v -e hardened -e uclib -e nomultilib -e systemd -e x32 -e vanilla| awk '{print $1}' | tail -n1)
-
-packer build -var cpu_brand="generic"       \
--var mem_size="4096"                        \
--var num_cpus="4"                           \
--var virt_type="vmware"                     \
--var mem_size="4096"                        \
--var march="native"                         \
--var make_opts="3"                          \
--var emerge_world="false"                   \
--var build_type="base"                      \
--var admin_user="$USER"                     \
--var stage3="$stage3"                       \
--var iso_checksum="$iso_sha512"             \
--var iso_url="http://distfiles.gentoo.org/releases/amd64/autobuilds/current-install-amd64-minimal/install-amd64-minimal-$stage3.iso" \
--var output_directory="$HOME/gentoo" ./vmware.json
+if [ -z "$1" ] ; then
+  packer build -var cpu_brand="generic"       \
+  -var mem_size="4096"                        \
+  -var num_cpus="4"                           \
+  -var virt_type="vmware"                     \
+  -var mem_size="4096"                        \
+  -var march="native"                         \
+  -var make_opts="3"                          \
+  -var emerge_world="false"                   \
+  -var build_type="base"                      \
+  -var admin_user="$USER"                     \
+  -var stage3="$stage3"                       \
+  -var iso_checksum="$iso_sha512"             \
+  -var iso_url="http://distfiles.gentoo.org/releases/amd64/autobuilds/current-install-amd64-minimal/install-amd64-minimal-$stage3.iso" \
+  -var output_directory="$HOME/gentoo" ./vmware.json
+fi
