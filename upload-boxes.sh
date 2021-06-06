03b564a55e8654254480040f6b72aceba31f78e6
Jesse Harris <zigford@gmail.com>
2021-06-06 10:15:23 +1000

    upload box auto

--- upload-boxes.sh
+++ upload-boxes.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#[[ -z "$VAGRANT_CLOUD_TOKEN" ]] && echo "No auth token found" && exit 1
+[[ -z "$VAGRANT_CLOUD_TOKEN" ]] && echo "No auth token found" && exit 1
 # setup auth
 AUTH_HEADER="Authorization: Bearer $VAGRANT_CLOUD_TOKEN"
 # read box
+BASE_URL="https://app.vagrantup.com/api/v1/box/zigford"
 
 get_box_prev_ver() {
     local box_name=$1
@@ -35,10 +36,20 @@ add_to_ver() {
     fi
 }
 
-new_ver() {
+add_provider_to_ver() {
+    local box_ver_url=$1
+    local provider=$2
+    echo "Creating new provider for $box_ver_url" >&2
+    curl -s --header "$AUTH_HEADER" \
+         --header "Content-Type: application/json" \
+         "${box_ver_url}/providers" \
+         --data "{ \"provider\": { \"name\": \"$provider\" } }" \
+         > /dev/null
+}
+
+create_new_ver_url() {
     local box_name=$1
-    oldver=$(get_box_prev_ver "$box_name")
-    newver=$(add_to_ver "$oldver" patch)
+    local newver=$2
     echo "Creating new version $newver" >&2
     curl -s --header "$AUTH_HEADER" \
          --header "Content-Type: application/json" \
@@ -47,35 +58,86 @@ new_ver() {
     jq -r '.release_url' | sed 's/\/\w\+$//'
 }
 
-new_provider() {
-    local box_ver_url=$1
-    local provider=$2
-    echo "Creating new provider for $box_ver_url" >&2
-    curl -s --header "$AUTH_HEADER" \
-         --header "Content-Type: application/json" \
-         "${box_ver_url}/providers" \
-         --data "{ \"provider\": { \"name\": \"$provider\" } }"
-}
-
 upload_box() {
-    response=$(curl \
-            --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-             https://app.vagrantup.com/api/v1/box/myuser/test/version/1.2.3/provider/virtualbox/upload)
-
+    local box="$1"
+    local box_name="$2"
+    local ver="$3"
+    local ver_url=${BASE_URL}/$box_name/version/$ver
+    local prov_url=${ver_url}/provider/libvirt/upload
+    new_ver_url=$(create_new_ver_url "$box_name" "$newver")
+    add_provider_to_ver "$new_ver_url" libvirt
+    response=$(curl -s --header "$AUTH_HEADER" $prov_url)
     # Extract the upload URL from the response (requires the jq command)
-
-    upload_path=$(echo "$response" | jq .upload_path)
-
-    # Perform the upload
-
-    curl $upload_path --request PUT --upload-file virtualbox-1.2.3.box
-
-
+    upload_path=$(echo "$response" | jq -r .upload_path)
+    curl $upload_path --request PUT --upload-file "$box"
+    if [[ $? == 0 ]]; then
+        curl --header "$AUTH_HEADER" "$new_ver_url" --request PUT
+    else
+        error_m "Something went wrong"
+    fi
 }
 
-ver_url=$(new_ver gentoo-openrc)
-new_provider $ver_url libvirt
+do_stuff() {
+    local box="$1"
+    local box_name="$2"
+    local update="$3"
+    oldver=$(get_box_prev_ver "$box_name")
+    newver=$(add_to_ver "$oldver" $update)
+    printf "Uploading $box as $box_name ver $oldver, "
+    printf "with new version $newver\n"
+    if [[ -n "$dry_run" ]]; then
+        echo "Dry run, skipping upload"
+        exit 0
+    fi
+    upload_box "$box" "$box_name" "$newver"
+
+}
 
 # get_box_prev_ver gentoo-openrc
+usage() {
+    echo "By Jesse Harris"
+    echo ""
+    echo "${0##*/} [options] box_file_name.box"
+    echo ""
+    echo "Options"
+    echo "-b, --box         specify the name of the box"
+    echo "-u, --update      specify major, minor, patch update"
+    echo "-n, --dry-run     dont do any work"
+    echo ""
+}
+
+error_m() {
+    local msg="$1"
+    RED="\e[31m"
+    ENDCOLOR="\e[0m"
+    echo ""
+    echo -e "${RED}${msg}${ENDCOLOR}"
+    echo ""
+    exit 1
+}
+
+main() {
+    local box
+    local name
+    local update
+    while [[ $# != 0 ]]; do
+        case "$1" in
+            --box-name|-b)  shift;name=$1;    shift;;
+            --update|-u)    shift;update=$1;  shift;;
+            --help|-h)      usage;            shift;;
+            --dry-run|-n)   dry_run=y;        shift;;
+            *)              box="$1";         shift;;
+        esac
+    done
+    
+    # validate params
+    [[ -z "$box" ]] || [[ ! -f "$box" ]] &&
+        error_m "Please specify box file"
+    update="${update:-major}"
+    name="${name:-$box}"
+    name="${name%.*}"
+    do_stuff "$box" "$name" "$update"
+}
 
 
+[[ "$BASH_SOURCE" == "$0" ]] && main "$@"
